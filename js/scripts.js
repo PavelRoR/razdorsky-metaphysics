5c165bc21e1065671311fa5e17a4e0b40fac8277
pavelp21 <pavelvp2182@yandex.ru>
2018-10-12 15:42:26 +0500

    Author is done

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,15 @@ $(document).ready(function() {
 
 
 
+
+
+/* Видео */
+$(function () {
+    $('.video_wrapper img').click(function () {
+        var dy = $(this).parent().attr('data-youtube');
+        $(this).parent().html('<iframe src="https://youtube.com/embed/' + dy +'?rel=0&autoplay=1" allowfullscreen></iframe>');
+    });
+});
+
 /*Конец документа*/
 });
